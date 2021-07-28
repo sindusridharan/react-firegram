9d84d3d1ee8e9635cd61e6dac5fe218e183a1326
Sindu Sridharan <ssri913@aucklanduni.ac.nz>
2021-07-29 07:15:20 +1200

    Updates

---
+++ precache-manifest.ef3d0deabf5f45f5465c4df43036a6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c213f00ca94d5af23dc8e745cb8f28",
+    "url": "/react-firegram/index.html"
+  },
+  {
+    "revision": "e3feaaf6d14730373756",
+    "url": "/react-firegram/static/css/main.43e6d642.chunk.css"
+  },
+  {
+    "revision": "8ddca9afa25421577088",
+    "url": "/react-firegram/static/js/2.762f4019.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/react-firegram/static/js/2.762f4019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3feaaf6d14730373756",
+    "url": "/react-firegram/static/js/main.8d8d7e02.chunk.js"
+  },
+  {
+    "revision": "6e41a62c830fae820770",
+    "url": "/react-firegram/static/js/runtime-main.c57774a6.js"
+  }
+]);
