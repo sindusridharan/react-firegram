b21c0af14fd1740911b9dd0047d6b1a0d2538a56
Sindu Sridharan <ssri913@aucklanduni.ac.nz>
2021-07-29 07:30:32 +1200

    other change

---
+++ build/precache-manifest.00f7607835a28365abeb32caf606c2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b2c01c13603fa65254a9abca518870",
+    "url": "/react-firegram/index.html"
+  },
+  {
+    "revision": "57a5672e95885b69a8a4",
+    "url": "/react-firegram/static/css/main.43e6d642.chunk.css"
+  },
+  {
+    "revision": "8ddca9afa25421577088",
+    "url": "/react-firegram/static/js/2.762f4019.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/react-firegram/static/js/2.762f4019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a5672e95885b69a8a4",
+    "url": "/react-firegram/static/js/main.fca3576f.chunk.js"
+  },
+  {
+    "revision": "6e41a62c830fae820770",
+    "url": "/react-firegram/static/js/runtime-main.c57774a6.js"
+  }
+]);
--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>PictureGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Welcome to PictureGram, select '+' to add your pictures</p>
     </div>
   )
 }
